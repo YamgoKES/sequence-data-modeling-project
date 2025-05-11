140322487a934e3c103395b0830133a890d87b25
YamgoKES <eunseokid@naver.com>
2025-05-11 05:19:56 -0400

    initial commit

---
+++ prediction/views.py
@@ -0,0 +1,6 @@
+from django.shortcuts import render
+
+# Create your views here.
+def home(request) :
+    context={'message' : "어획량 예측서비스"}
+    return render(request, 'prediction/home.html', context)
