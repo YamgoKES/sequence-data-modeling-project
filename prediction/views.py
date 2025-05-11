17be952976a9738d940e74af8ee921c09dcdb2d5
YamgoKES <eunseokid@naver.com>
2025-05-11 05:45:12 -0400

    feat : mr test

--- prediction/views.py
+++ prediction/views.py
@@ -2,5 +2,5 @@ from django.shortcuts import render
 
 # Create your views here.
 def home(request) :
-    context={'message' : "어획량 예측서비스"}
+    context={'message' : "어획량 예측 서비스"}
     return render(request, 'prediction/home.html', context)
